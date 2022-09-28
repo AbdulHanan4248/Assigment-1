56537ab39f11c3b4af1e3f9eafd6a78bbf923258
AbdulHanan4248 <abdulhananrauf4248@gmail.com>
2022-09-28 10:34:21 +0900

    your comment

---
+++ app.ts
@@ -0,0 +1,137 @@
+ //length of string
+/*var str = "Hello everyone, how are you,Are you fine";
+console.log(str.length);
+//copy string
+var str:string =  "Mangoes"
+var copy = str;
+console.log(copy);
+var num:number = 15;
+var style = num;
+console.log(style);*/
+// reverse of string
+/*var mun = ["Bilal","Moazzam","Muneeb","Hassan"];
+mun.reverse();
+console.log(mun);
+//concatinat of string
+var bbc = ["Bilal","Moazzam","Muneeb","Hassan"]
+var ccn = bbc.concat("Hanan","Ali","Majeed")
+var bbn = ["Abu Baker","umer","Usman"]
+var j = bbn.concat(ccn)
+console.log(j);
+
+
+//console.log(ccn);*/
+
+//var str:string = "   Typescript    is   best  language   "
+//var g = str.trim()
+//var d = str.replace(/is/g,"are")
+//console.log(g);
+//var s = str.trim()
+ //alert(s)
+
+ //switch case programmer//
+ //var day = 6;
+ //switch (day) {
+   /* case 0:
+        alert("Today is Monday");
+        
+        break;
+        case 1:
+            alert("Today is Tuesday");
+            
+            break;
+            case 2:
+                alert("Today is Wednesday")
+                
+            break;
+            case 3:
+                alert("Today is Thursday")
+            break;
+            case 4:
+                alert("Today is Friday")
+            break;
+            case 5:
+                alert(" Today is Saturday")
+            break;
+            case 6:
+                alert("Today is Sunday")
+            break;
+ 
+    default:
+        break;
+ }*/
+ /*var x = 11;
+ 
+ switch (x%2==0) {
+    case true:
+        console.log(x +"Number is even");
+        break;
+ case false:
+ console.log( x + "Number is odd");
+ break;
+ 
+    default:
+        //console.log("This not valid");
+        
+        //console.log("Number is odd");
+        
+        break;
+ } 
+var inputnumber:number = 2008;
+switch (inputnumber%4==0) {
+    case true:
+        console.log("This is leap year");
+        
+        
+        break;
+        case false:
+            console.log("This is not leap year");
+            break;
+    default:
+        break;
+}*/
+/*
+var Num2:Number = 25;
+var Num3:Number = 28;
+switch (Num2 > Num3) {
+    case true:
+        console.log("Num2 is greater than num3");
+        
+        
+        break;
+case false:
+    console.log("Num2 is not greater than num3");
+    
+    break;
+    default:
+        break;
+}
+var inputnumber:number = -2;
+switch (inputnumber>=0) {
+    case true:
+        console.log("Number is postivie");
+        
+        
+        break;
+case false:
+    console.log("Number is Negitive");
+    
+    default:
+        break;
+}
+var str = "u";
+switch (str=='a'|| str=='i'|| str=='o' || str=='u' 
+|| str=="e" ) {
+    case true:
+        console.log("it is vowel word");
+        
+        
+        break;
+        case false:
+            console.log("This is not vowel word");
+            
+            break;
+
+    default:
+        break;
+}*/
